ff66246952943ae7158485f5a7e9af51a5cf0537
Clément Storne <clementstorne@gmail.com>
2024-06-25 01:09:45 +0200

    countdown is completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,7 +21,15 @@ export default function RootLayout({
     <html lang="fr">
       <body>
         <Navbar />
-        {children}
+        <main
+          className={cn(
+            "max-w-240 min-h-screen mb-8 flex flex-col items-center justify-center space-y-8",
+            "mx-5 mt-100",
+            "md:mx-auto md:mt-146"
+          )}
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
